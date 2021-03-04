4a5608d70e444a068597c6c2b22c2a48561d3801
Viacheslav <titaniumslava@gmail.com>
2021-03-04 18:45:21 +0400

    Добавил настройки для Celery

--- backend/books_exchange/books_exchange/settings.py
+++ backend/books_exchange/books_exchange/settings.py
@@ -153,3 +153,6 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
